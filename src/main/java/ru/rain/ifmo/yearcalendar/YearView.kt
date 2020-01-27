ccad79df9f4b63145cecb6a082f345ecd9c7ee58
Aroize <gipermonk@bk.ru>
2020-01-27 19:09:27 +0300

    fixed title text within YearView.kt

--- src/main/java/ru/rain/ifmo/yearcalendar/YearView.kt
+++ src/main/java/ru/rain/ifmo/yearcalendar/YearView.kt
@@ -2,9 +2,9 @@ package ru.rain.ifmo.yearcalendar
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.core.view.get
 import androidx.gridlayout.widget.GridLayout
 import java.time.Year
@@ -20,6 +20,8 @@ class YearView(context: Context,
 
     private val mainView = View.inflate(context, R.layout.year_view, null)
 
+    private val yearTitle = mainView.findViewById<TextView>(R.id.textView).apply { text = adapter.title() }
+
     private val gridLayout = mainView.findViewById<GridLayout>(R.id.gridLayout_year)
 
     init {
@@ -34,6 +36,7 @@ class YearView(context: Context,
     }
 
     private fun notifyDataChanged() {
+        yearTitle.text = adapter.title()
         for (i in 1..12) {
             val month = adapter.year.atMonth(i)
             (gridLayout[i - 1] as MonthView).adapter.month = month
@@ -41,6 +44,9 @@ class YearView(context: Context,
     }
 
     open inner class YearAdapter(year: Year) {
+
+        open fun title() = year.value.toString()
+
         var year = year
         set(value) {
             field = value
